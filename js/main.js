1631037786738cd062527e8d40cba2e403f06dd0
clarkfourie <gacfourie@live.com>
2016-12-20 20:20:56 +0200

    still at part 6, issues with the elements

--- js/main.js
+++ js/main.js
@@ -1,19 +1,10 @@
 var app = {
 
-    findByName: function () {
-        var self = this;
-        this.store.findByName($('.search-key').val(), function (employees) {
-            $('.employee-list').html(self.employeeLiTpl(employees));
-        });
-    },
-
     initialize: function () {
-        var self = this; //NOTE have to make a new instance of self to call renderHomeView while using this
+        var self = this;
         this.store = new MemoryStore(function () {
-            self.renderHomeView();
+            $('body').html(new HomeView(self.store).render().el);
         });
-        this.homeTpl = Handlebars.compile($("#home-tpl").html());
-        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
     },
 
     showAlert: function (message, title) {
@@ -22,22 +13,7 @@ var app = {
         } else {
             alert(title ? (title + ": " + message) : message);
         }
-    },
-
-    renderHomeView: function () {
-        $('body').html(this.homeTpl());
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    },
-
-    //  NOTE: Use to test showAlert
-    //    initialize: function () {
-    //        var self = this;
-    //        this.store = new MemoryStore(function () {
-    //            self.showAlert('Store Initialized', 'Info');
-    //        });
-    //        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    //    }
-
+    }
 };
 
 app.initialize();
