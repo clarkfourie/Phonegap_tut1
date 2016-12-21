6c6abce7c723a5eedd5b4b67f028c4a0736e5d70
clark <gacfourie@live.com>
2016-12-21 09:49:06 +0200

    Part 6 complete

--- js/homeview.js
+++ js/homeview.js
@@ -1,23 +1,24 @@
 var HomeView = function (store) {
 
     this.initialize = function () {
-            // Define a div wrapper for the view. The div wrapper is used to attach events.
-            this.el = $("<div/>");
-            this.el.on('keyup', '.search-key', this.findByName);
-        },
-
-        this.initialize();
+        // Define a div wrapper for the view. The div wrapper is used to attach events.
+        this.el = $('<div/>');
+        $(document).on("keyup", ".search-key", this.findByName);
+    };
 
     this.render = function () {
-            this.el.html(HomeView.template());
-            return this;
-        },
+        this.el.html(HomeView.template());
+        return this;
+    };
 
     this.findByName = function () {
         store.findByName($('.search-key').val(), function (employees) {
             $('.employee-list').html(HomeView.liTemplate(employees));
         });
-    }
+    };
+
+    // NOTE: this.initialize() must always be called at the end
+    this.initialize();
 }
 
 HomeView.template = Handlebars.compile($("#home-tpl").html());
